6f1e32d4c326b12f617d8969a70b42750b2b9308
Xio41 <scjc4190@gmail.com>
2025-03-12 18:34:21 -0600

    Se añadio lo de las listas negras

--- index.py
+++ index.py
@@ -7,7 +7,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.models.user import User
 from app.routes import auth
 from app.db.database import get_db 
-
 app = FastAPI()
 app.include_router(auth.router)
 
